820e16ccca65680fe5d5b64b3c40d78bf71eec39
Divesh011 <diveshsingh2005@gmail.com>
2025-09-08 00:05:50 +0530

    Added UI Element: in MyBlogsFragment to tell user that he has not uploaded any Blog.

--- app/src/main/java/com/divcode/bloggit/Fragments/MyBlogsFragment.kt
+++ app/src/main/java/com/divcode/bloggit/Fragments/MyBlogsFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,6 +19,7 @@ class MyBlogsFragment(): Fragment() {
     private lateinit var myBlogRecyclerView: RecyclerView
 
     private lateinit var adapter: MyBlogsAdapter
+    private lateinit var infoTv: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,7 +27,6 @@ class MyBlogsFragment(): Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_my_blogs, container, false)
-
         val myBlogs = mutableListOf<Blog>()
 
         CommonUtils.loadedBlogs.forEach {
@@ -38,10 +39,18 @@ class MyBlogsFragment(): Fragment() {
         Log.d("MYBLOGFRAG", myBlogs.toString())
 
         myBlogRecyclerView = view.findViewById(R.id.my_blogs_rc)
+        infoTv = view.findViewById(R.id.NoBlogsTv)
+
+        if (myBlogs.isNotEmpty()){
+            infoTv.visibility = View.INVISIBLE
+        }
+
         adapter = MyBlogsAdapter(myBlogs,requireContext())
         myBlogRecyclerView.adapter = adapter
         myBlogRecyclerView.layoutManager = LinearLayoutManager(context)
         myBlogRecyclerView.setHasFixedSize(true)
+
+
 
         return view
     }
